538ad1573b996cbfd9d592f70a92f077f057a8a6
darshan <kmdarshan@gmail.com>
2015-05-21 01:01:29 -0700

    adding cell

---
+++ ContactSelector/contacts/RRContactTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  RRContactTableViewCell.h
+//  ContactSelector
+//
+//  Created by darshan on 5/21/15.
+//  Copyright (c) 2015 Happy Days. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RRContactTableViewCell : UITableViewCell
+
+@end
---
+++ ContactSelector/contacts/RRContactTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  RRContactTableViewCell.m
+//  ContactSelector
+//
+//  Created by darshan on 5/21/15.
+//  Copyright (c) 2015 Happy Days. All rights reserved.
+//
+
+#import "RRContactTableViewCell.h"
+
+@implementation RRContactTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end

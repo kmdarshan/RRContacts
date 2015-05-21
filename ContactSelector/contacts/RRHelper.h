53d700e176aa79327b2c9becfed0bdb5f99a1ae2
darshan <kmdarshan@gmail.com>
2015-05-20 22:18:40 -0700

    fixing warnings

--- ContactSelector/contacts/RRHelper.h
+++ ContactSelector/contacts/RRHelper.h
@@ -11,23 +11,23 @@
 typedef void(^RRCallback)(BOOL success, id result);
 
 #pragma mark - font
-static const NSString *kFontRegular = @"Arimo";
-static const NSInteger kMainViewPaddingX  = 8.0;
-static const CGFloat kFontNormalSize = 18.0f;
-static const CGFloat kFontSizeListingTextLabel = 16.0f;
-static const CGFloat kFontSizeListingDetailedTextLabel = 12.0f;
+static NSString *kFontRegular = @"Arimo";
+static NSInteger kMainViewPaddingX  = 8.0;
+static CGFloat kFontNormalSize = 18.0f;
+static CGFloat kFontSizeListingTextLabel = 16.0f;
+static CGFloat kFontSizeListingDetailedTextLabel = 12.0f;
 
 #pragma mark - height
-static const NSInteger kRowHeight = 55;
-static const NSInteger kRowHeightEventDescriptionCell = 205;
+static NSInteger kRowHeight = 55;
+static NSInteger kRowHeightEventDescriptionCell = 205;
 
 #pragma mark - notifications
-static const NSString *RRNotificationSelectFriends = @"com.redflower.addEvents.addFriendsViewController";
+static NSString *RRNotificationSelectFriends = @"com.redflower.addEvents.addFriendsViewController";
 
 #pragma mark - names
-static const NSString *kAddEvent = @"ADD EVENT";
-static const NSString *kAddFriends = @"Friends";
-static const NSString *kInviteByEmail = @"INVITE BY EMAIL";
+static NSString *kAddEvent = @"ADD EVENT";
+static NSString *kAddFriends = @"Friends";
+static NSString *kInviteByEmail = @"INVITE BY EMAIL";
 
 #pragma mark - paddings
 @interface RRHelper : NSObject

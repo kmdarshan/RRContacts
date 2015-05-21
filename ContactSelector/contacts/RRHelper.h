96b29ea266c419780f387c1b27db70d7dc17c58e
darshan <kmdarshan@gmail.com>
2015-05-20 21:18:33 -0700

    cleaning up code

--- ContactSelector/contacts/RRHelper.h
+++ ContactSelector/contacts/RRHelper.h
@@ -11,16 +11,10 @@
 typedef void(^RRCallback)(BOOL success, id result);
 
 #pragma mark - font
-static NSString *kFontBold = @"Arimo-Bold";
-static NSString *kFontBoldItalic = @"Arimo-BoldItalic";
-static NSString *kFontItalic = @"Arimo-Italic";
 static NSString *kFontRegular = @"Arimo";
 static NSInteger mainViewPaddingX = 8.0;
 static NSInteger mainViewPaddingY = 8.0;
-static NSInteger kStatusBarHeight = 40;
 static CGFloat kFontNormalSize = 18.0f;
-static CGFloat kFontNavigationBarSize = 18.0f;
-static CGFloat kFontSmallSize = 12.0f;
 static CGFloat kFontSizeListingTextLabel = 16.0f;
 static CGFloat kFontSizeListingDetailedTextLabel = 12.0f;
 #pragma mark - height
@@ -33,13 +27,7 @@ static NSString *notificationSelectFriends = @"com.redflower.addEvents.addFriend
 static NSString *kAddEvent = @"ADD EVENT";
 static NSString *kAddFriends = @"Friends";
 static NSString *kInviteByEmail = @"INVITE BY EMAIL";
-#pragma mark - object names
-static NSString *kEventDetails = @"eventDetails";
-#pragma mark - verbs
-static NSString *kBy = @" By ";
-static NSString *kDone = @"DONE";
 #pragma mark - paddings
-static NSInteger kPaddingEventDetailsTableViewCells = 2.0f;
 @interface RRHelper : NSObject
 +(UIImage*) resizeImage:(UIImage *)image toSize:(CGSize)size;
 +(UIColor*) appBlue;

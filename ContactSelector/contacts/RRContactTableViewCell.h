08142bad14dbcf82d39a0f6688a1929c186275ca
darshan <kmdarshan@gmail.com>
2015-05-21 01:01:56 -0700

    adding cells

--- ContactSelector/contacts/RRContactTableViewCell.h
+++ ContactSelector/contacts/RRContactTableViewCell.h
@@ -9,5 +9,4 @@
 #import <UIKit/UIKit.h>
 
 @interface RRContactTableViewCell : UITableViewCell
-
 @end
--- ContactSelector/contacts/RRContactTableViewCell.m
+++ ContactSelector/contacts/RRContactTableViewCell.m
@@ -9,15 +9,4 @@
 #import "RRContactTableViewCell.h"
 
 @implementation RRContactTableViewCell
-
-- (void)awakeFromNib {
-    // Initialization code
-}
-
-- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
-    [super setSelected:selected animated:animated];
-
-    // Configure the view for the selected state
-}
-
 @end
